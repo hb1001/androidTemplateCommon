7be482410536b753340ef655d6181d69bbbd3edd
zhangddq <1941670713@qq.com>
2025-11-01 00:15:00 +0800

    稍微解决测试的报错。（可选）

--- build-logic/src/main/kotlin/AndroidLibraryConventionPlugin.kt
+++ build-logic/src/main/kotlin/AndroidLibraryConventionPlugin.kt
@@ -1,10 +1,11 @@
-// File: build-logic/src/main/kotlin/AndroidLibraryConventionPlugin.kt
-
 import com.android.build.api.dsl.LibraryExtension
 import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
 import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
 
 class AndroidLibraryConventionPlugin : Plugin<Project> {
@@ -22,6 +23,8 @@ class AndroidLibraryConventionPlugin : Plugin<Project> {
                 defaultConfig {
                     minSdk = 24
                     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+                    // 添加 consumer-proguard-rules.pro  应该是可选的
+//                    consumerProguardFiles("consumer-rules.pro")
                 }
                 compileOptions {
                     sourceCompatibility = JavaVersion.VERSION_17
@@ -42,6 +45,18 @@ class AndroidLibraryConventionPlugin : Plugin<Project> {
             extensions.configure<KotlinAndroidProjectExtension> {
                 jvmToolchain(17)
             }
+
+            // --- 新增：统一添加测试依赖 --- 暂时注释因为其他的都写了
+//            val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+//            dependencies {
+//                // 单元测试
+//                add("testImplementation", libs.findLibrary("junit").get())
+//
+//                // UI/仪器测试
+//                add("androidTestImplementation", libs.findLibrary("androidx.junit").get())
+//                add("androidTestImplementation", libs.findLibrary("androidx.espresso.core").get())
+//            }
+            // --- 新增结束 ---
         }
     }
 }
--- feature-solver/build.gradle.kts
+++ feature-solver/build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 
     // Hilt
     implementation(libs.hilt.android)
+    implementation(libs.androidx.monitor)
     kapt(libs.hilt.compiler)
     implementation(libs.hilt.navigation.compose)
 
